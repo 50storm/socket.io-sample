74b58470b728dde6f6627856b3fec25639fe635c
Igarashi <aaaaaaa@aaaa.com>
2019-03-07 15:49:25 +0900

    save

--- index.js
+++ index.js
@@ -1,32 +1,51 @@
 // npm install --save express@4.15.2
 // npm install --save socket.io
-
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-
-app.get('/', function(req, res){
-    console.log(__dirname);
-  res.sendFile(__dirname + '/index.html');
-});
-
-io.on('connection', function(socket){
-  console.log('a user connected');
-  // console.log(socket);
-    io.emit("chat message", "Hi! New User!")  //送信元含む全員に送信
-    socket.on('chat message', function(msg){
-        io.emit('chat message', msg); //送信元含む全員に送信
-        // socket.broadcast.emit("chat message", msg)　//送信元以外の全員に送信
+// S01. 必要なモジュールを読み込む
+var http = require('http');
+var socketio = require('socket.io');
+var fs = require('fs');
+// S02. HTTPサーバを生成する
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type' : 'text/html'});
+    res.end(fs.readFileSync(__dirname + '/index.html', 'utf-8'));
+}).listen(3000);  // ポート競合の場合は値を変更
+ 
+// S03. HTTPサーバにソケットをひも付ける（WebSocket有効化）
+var io = socketio.listen(server);
+ 
+// S04. connectionイベントを受信する
+io.sockets.on('connection', function(socket) {
+    var room = '';
+    var name = '';
+ 
+    // roomへの入室は、「socket.join(room名)」
+    socket.on('client_to_server_join', function(data) {
+        room = data.value;
+        socket.join(room);
     });
-
-    socket.on('disconnect', function(){
-        console.log('user disconnected');
+    // S05. client_to_serverイベント・データを受信する
+    socket.on('client_to_server', function(data) {
+        // S06. server_to_clientイベント・データを送信する
+        io.to(room).emit('server_to_client', {value : data.value});
     });
-
+    // S07. client_to_server_broadcastイベント・データを受信し、送信元以外に送信する
+    socket.on('client_to_server_broadcast', function(data) {
+        socket.broadcast.to(room).emit('server_to_client', {value : data.value});
+    });
+    // S08. client_to_server_personalイベント・データを受信し、送信元のみに送信する
+    socket.on('client_to_server_personal', function(data) {
+        var id = socket.id;
+        name = data.value;
+        var personalMessage = "あなたは、" + name + "さんとして入室しました。"
+        io.to(id).emit('server_to_client', {value : personalMessage});
+    });
+    // S09. dicconnectイベントを受信し、退出メッセージを送信する
+    socket.on('disconnect', function() {
+        if (name == '') {
+            console.log("未入室のまま、どこかへ去っていきました。");
+        } else {
+            var endMessage = name + "さんが退出しました。"
+            io.to(room).emit('server_to_client', {value : endMessage});
+        }
+    });
 });
-
-
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
-
